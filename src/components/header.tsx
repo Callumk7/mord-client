983139ee5c3c124fd124a4c614604476ee9f3e99
Callum Kloos <callumkloos@gmail.com>
2025-11-21 14:05:14 +0000

    fix navigation menu?

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,31 +1,73 @@
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
-import { campaignsQueryOptions } from "~/query/options";
+import {
+	campaignsQueryOptions,
+	campaignWarbandQueryOptions,
+} from "~/query/options";
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuPositioner,
+	NavigationMenuPopup,
+	NavigationMenuTrigger,
+} from "./ui/navigation-menu";
 
 interface HeaderProps {
 	campaignId: string;
 }
 export function Header({ campaignId }: HeaderProps) {
+	const { data: warbands } = useQuery(
+		campaignWarbandQueryOptions(Number(campaignId)),
+	);
 	return (
-		<header className="p-4 flex justify-between items-center bg-card border-b shadow-lg">
-			<div className="flex items-center gap-4">
-				<Link to="/$campaign" params={{ campaign: campaignId }}>
-					Leaderboard
-				</Link>
-				<Link to="/$campaign/warbands" params={{ campaign: campaignId }}>
-					Warbands
-				</Link>
-				<Link to="/$campaign/matches" params={{ campaign: campaignId }}>
-					Matches
-				</Link>
-			</div>
-			<div className="flex items-center gap-4">
-				<Link to="/$campaign/admin" params={{ campaign: campaignId }}>
-					Admin
-				</Link>
-				<Link to="/reference">Reference</Link>
-			</div>
-		</header>
+		<NavigationMenu>
+			<NavigationMenuList>
+				<NavigationMenuItem>
+					<NavigationMenuLink
+						render={<Link to="/$campaign" params={{ campaign: campaignId }} />}
+					>
+						Leaderboard
+					</NavigationMenuLink>
+				</NavigationMenuItem>
+				<NavigationMenuItem>
+					<NavigationMenuTrigger>Test</NavigationMenuTrigger>
+					<NavigationMenuContent>
+						<NavigationMenuLink>First Item</NavigationMenuLink>
+						<NavigationMenuLink>Second Item</NavigationMenuLink>
+						<NavigationMenuLink>Third Item</NavigationMenuLink>
+					</NavigationMenuContent>
+				</NavigationMenuItem>
+				{warbands && (
+					<NavigationMenuItem>
+						<NavigationMenuTrigger>Warbands</NavigationMenuTrigger>
+						<NavigationMenuContent>
+							{warbands.map((warband) => (
+								<NavigationMenuLink
+									key={warband.id}
+									render={
+										<Link
+											to="/$campaign/warbands/$warband"
+											params={{
+												campaign: campaignId,
+												warband: warband.id.toString(),
+											}}
+										/>
+									}
+								>
+									{warband.name}
+								</NavigationMenuLink>
+							))}
+						</NavigationMenuContent>
+					</NavigationMenuItem>
+				)}
+			</NavigationMenuList>
+			<NavigationMenuPositioner>
+				<NavigationMenuPopup />
+			</NavigationMenuPositioner>
+		</NavigationMenu>
 	);
 }
 
